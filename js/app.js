3a32a8890754a7046eee5bc198374ab691939969
Jeremy Lee <www.jeremy.llee27@gmail.com>
2019-02-14 14:53:05 -0800

    Pushing up Thursday's updates on a new branch

--- js/app.js
+++ js/app.js
@@ -1,36 +1,60 @@
 'use strict'
-
-//Setting global variable for the hours of the day to keep code DRY
-var hoursOfOperation = [
-    '6am: ',
-    '7am: ',
-    '8am: ',
-    '9am: ',
-    '10am: ',
-    '11am: ',
-    '12pm: ',
-    '1pm: ',
-    '2pm: ',
-    '3pm: ',
-    '4pm: ',
-    '5pm: ',
-    '6pm: ',
-    '7pm: ',
-    '8pm: ',
-]
-
+// Selecting a ul by it class name via the DOM 
 var pikeUI = document.getElementsByClassName('first-pike');
 console.log(pikeUI);
 
+//Creating an object to hold properties for the 1st and Pike store location.
 var firtPikeStore = {
+    // Key for given values by the client: avg maxium customer, avg minium customer, and avg cookies sold.
     minCust: 23,
     maxCust: 65,
     avgCookiesSold: 6.3,
 
-    hoursOfOperation: '',
-
-    calculate: function(min, max) {
-        return Math.round(Math.random() * (this.max - this.min + 1) - this.min);
+    //Creating a key for the store business hours.
+    hoursOfOperation: [
+        '6am: ',
+        '7am: ',
+        '8am: ',
+        '9am: ',
+        '10am: ',
+        '11am: ',
+        '12pm: ',
+        '1pm: ',
+        '2pm: ',
+        '3pm: ',
+        '4pm: ',
+        '5pm: ',
+        '6pm: ',
+        '7pm: ',
+        '8pm: ',
+    ], 
+    
+    // Making a method and passing in min and max customers to generate a random number between a specified range.  
+    makeRandom: function(minCust, maxCust) {
+        // Returning the calculation of the randomized number.
+        return Math.round(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
     },
 
+    // Empty key array to store the calculation of the average cookies sold per hour.
+    cookiesPerHour: [],
+    // Key set to zero to store the calculation of the random customers and the cookies sold.
+    cookiesSold: 0,
+
+    // Making a method for the calculation of the cookies sold per hour.
+    calculate: function() {
+        for(var i = 0; i < this.hoursOfOperation.length; i++) {
+            // Making variable for randomized customers per hour.
+            var randomCustomer = this.makeRandom();
+            // Calculating cookies sold per hour, t.aCS = 6.3 * 100 / the amount of randomized customers.
+            this.cookiesSold = Math.floor(this.avgCookiesSold * 100 / randomCustomer);
+            // Pushing the calculation of the cookies sold to store in an empty key array.
+            this.cookiesPerHour.push(this.cookiesSold);
+        }
+    } 
 }
+// Logging various properties for visual purposes.
+console.log("Calculate function " + firtPikeStore.calculate());
+console.log("Cookies per hour: " + firtPikeStore.cookiesPerHour);
+console.log("Random number: " + firtPikeStore.makeRandom());
+console.log("Hour of operation: " + firtPikeStore.hoursOfOperation);
+console.log("Cookies sold in one hour "+ firtPikeStore.cookiesSold);
