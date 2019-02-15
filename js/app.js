28e764f8e07bbf56b3c520374d65a0cb42e4b7f2
Jeremy Lee <www.jeremy.llee27@gmail.com>
2019-02-14 22:34:31 -0800

    Pushing up updates for the end of the day.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict'
 // Selecting a ul by it class name via the DOM 
-var pikeUI = document.getElementsByClassName('first-pike');
+var pikeUI = document.getElementById('first-pike');
 console.log(pikeUI);
 
 //Creating an object to hold properties for the 1st and Pike store location.
@@ -12,27 +12,27 @@ var firtPikeStore = {
 
     //Creating a key for the store business hours.
     hoursOfOperation: [
-        '6am: ',
-        '7am: ',
-        '8am: ',
-        '9am: ',
-        '10am: ',
-        '11am: ',
-        '12pm: ',
-        '1pm: ',
-        '2pm: ',
-        '3pm: ',
-        '4pm: ',
-        '5pm: ',
-        '6pm: ',
-        '7pm: ',
-        '8pm: ',
+        '6am',
+        '7am',
+        '8am',
+        '9am',
+        '10am',
+        '11am',
+        '12pm',
+        '1pm',
+        '2pm',
+        '3pm',
+        '4pm',
+        '5pm',
+        '6pm',
+        '7pm',
+        '8pm',
     ], 
     
     // Making a method and passing in min and max customers to generate a random number between a specified range.  
     makeRandom: function(minCust, maxCust) {
         // Returning the calculation of the randomized number.
-        return Math.round(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+        return Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
     },
 
     // Empty key array to store the calculation of the average cookies sold per hour.
@@ -46,15 +46,27 @@ var firtPikeStore = {
             // Making variable for randomized customers per hour.
             var randomCustomer = this.makeRandom();
             // Calculating cookies sold per hour, t.aCS = 6.3 * 100 / the amount of randomized customers.
-            this.cookiesSold = Math.floor(this.avgCookiesSold * 100 / randomCustomer);
+            this.cookiesSold = Math.floor(this.avgCookiesSold * randomCustomer);
             // Pushing the calculation of the cookies sold to store in an empty key array.
             this.cookiesPerHour.push(this.cookiesSold);
         }
-    } 
+    }, 
+    
+    render: function () {
+        this.calculate();
+        for(var i = 0; i < this.hoursOfOperation.length; i++) {
+            var firstPikeLi = document.createElement('li');
+            firstPikeLi.textContent = `${this.hoursOfOperation[i]}: ${this.cookiesPerHour[i]}.`;
+            pikeUI.appendChild(firstPikeLi);
+        }
+    }
 }
+
+firtPikeStore.render();
 // Logging various properties for visual purposes.
 console.log("Calculate function " + firtPikeStore.calculate());
 console.log("Cookies per hour: " + firtPikeStore.cookiesPerHour);
 console.log("Random number: " + firtPikeStore.makeRandom());
 console.log("Hour of operation: " + firtPikeStore.hoursOfOperation);
 console.log("Cookies sold in one hour "+ firtPikeStore.cookiesSold);
+console.log("Render: " + firtPikeStore.render);
